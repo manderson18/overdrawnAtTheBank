b72a16a6ad8f540c04daa277c1fbf0f07a014f27
Maxwell Anderson <spamformaxwell@gmail.com>
2017-10-04 09:30:25 -0400

    Updated Main.java
    Created a skeleton in the if statements for scanner input.
    Next step is to actually create the methods needed to make the if statement work.
    Final step is to allow for the user to withdraw/deposit in the same session without having to come back

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,13 +12,21 @@ public class Main {
 
         bankAccount myAccount = new bankAccount(balance, name);
         System.out.println("Welcome " + myAccount.returnName() + " Your balance is " + myAccount.returnBalance()+" What would you like to do? ");
-        String response = kbin.next();
-        if(response.equalsIgnoreCase("withdraw")){
-            System.out.println(); // Withdraw money, and tell  user how much they withdrew, and their remaining balance.
-        } else if(response.equalsIgnoreCase("deposit"))
+        String bufferResponse = kbin.nextLine();
+	    
+	    String response = kbin.nextLine();
+        if(response.equalsIgnoreCase("withdraw"))
+	{
+		System.out.println("OK "+myAccount.returnName+" you have just withdrawn"+withdrawAmount+" your remaining balance is "+balanceAfterWithdraw+". Have a good day!"); // Withdraw money, and tell the user how much they withdrew, and their remaining balance.
+        } 
+	    else if(response.equalsIgnoreCase("deposit"))
         {
-
-            System.out.println(); // Deposit money, tell user how much they deposited, and their remaining balance.
+            System.out.println("OK "+myAccount.returnName+" you have just deposited"+depositAmount+" your new and improved balance is "+balanceAfterDeposit+". Have a good day!"); // Deposit money, tell user how much they deposited, and their remaining balance.
         }
+	    
+    } else 
+    
+    {
+    System.out.println("Okay, have a good day "+ myAccount.returnName +'.');
     }
 }
