b1bf4e02c86209dae9d67af0fb74de91623c9cea
Maxwell Anderson <spamformaxwell@gmail.com>
2017-10-05 10:16:27 -0400

    Update Main.java
    Created experimental while and do-while statements that should act as a primitive overdraft prevention, and loop system.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,9 +13,9 @@ public class Main {
         bankAccount myAccount = new bankAccount(balance, name);
         System.out.println("Welcome " + myAccount.returnName() + " Your balance is " + myAccount.returnBalance()+" What would you like to do? \n Deposit \n OR \n Withdraw ");
         String bufferResponse = kbin.nextLine();
-
+while(int userExit = 0){
 	    String response = kbin.nextLine();
-
+do{
         if(response.equalsIgnoreCase("withdraw"))
 	{
         System.out.println("Please enter the amount you wish to withdraw: ");
@@ -23,15 +23,18 @@ public class Main {
 
         System.out.println("OK, "+myAccount.returnName()+", you have just withdrawn "+myAccount.withdraw(withdrawAmount)+". Your remaining balance is "+myAccount.returnBalance()+". Have a good day!"); // Withdraw money, and tell the user how much they withdrew, and their remaining balance.
         }
+}while(myAccount.withdraw > myAccount.returnBalance);
 	    else if(response.equalsIgnoreCase("deposit"))
         {
             System.out.println("Please enter the amount you wish to deposit: ");
             double depositAmount = kbin.nextInt();
             System.out.println("OK, "+myAccount.returnName()+", you have just deposited "+myAccount.deposit(depositAmount)+". Your new and improved balance is "+myAccount.returnBalance()+". Have a good day!"); // Deposit money, tell user how much they deposited, and their remaining balance.
         } else{
+		    userExit = 1;
             System.out.println("Okay, have a great day "+ myAccount.returnName());
         }
 
+    }
     }
 }
 // Add a while statement to allow multiple transactions
